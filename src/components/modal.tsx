6c4e74c831143d949d74fd721b084bb326256b17
bruno <bruno@brunos-MacBook-Pro.local>
2024-07-13 16:29:19 -0300

    first commit

---
+++ src/components/modal.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { tv, VariantProps } from 'tailwind-variants';
+
+const modalVariants = tv({
+  base: 'rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5',
+  variants: {
+    size: {
+      default: 'w-[640px]',
+      auto: '',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
+
+interface ModalProps extends VariantProps<typeof modalVariants> {
+  children: ReactNode;
+}
+
+export function Modal({ children, size }: ModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className={modalVariants({ size })}>{children}</div>
+    </div>
+  );
+}
